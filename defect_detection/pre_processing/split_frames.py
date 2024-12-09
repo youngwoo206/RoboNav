7b49aaaaee9e8f1fad907d0e1c4624e37b345c9c
Anirudh Kilambi <anirudh.kilambi@compassdigital.io>
2024-12-09 18:38:42 -0500

    clean up split frames path to use final path for write out

--- defect_detection/pre_processing/split_frames.py
+++ defect_detection/pre_processing/split_frames.py
@@ -10,12 +10,13 @@ for video in videos:
     print(f"Processsing Video: {video}")
     prefix = video.split(".")[0]
     path = f"{workdir}{video}"
-    os.mkdir(f"/home/anirudh/code/repos/pipe-defect-detection/raw/{prefix}/")
+    final_path = f"/home/anirudh/code/repos/RoboNav/defect_detection/raw_images/{prefix}/"
+    os.mkdir(final_path)
     vidcap = cv2.VideoCapture(path)
     success,image = vidcap.read()
     count = 1
     while success:
-        cv2.imwrite(f"/home/anirudh/code/repos/pipe-defect-detection/raw/{prefix}/{count}.jpg", image)
+        cv2.imwrite(f"final_path/{count}.jpg", image)
         success, image = vidcap.read()
         count += 1
 
